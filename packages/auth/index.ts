3249124784ebcf71ad0b7e48f00d9fc7884683ec
Krindendo work <lollipopfunkyx9@gmail.com>
2023-08-29 20:38:44 +0200

    minor update

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,3 +1,17 @@
 import { db, tableCreator } from "@hermes/db";
 
 import { env } from "./env.mjs";
+
+export interface Session {
+  user: {
+    id: string;
+  };
+}
+
+export function GET() {}
+
+export function POST() {}
+
+export function auth() {
+  return { user: { id: "test" } } as Session;
+}
