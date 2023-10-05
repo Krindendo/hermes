75f2d1cad50b19336aaee2a5f70a5f23f3836c02
Krindendo work <lollipopfunkyx9@gmail.com>
2023-10-06 00:55:41 +0200

    added new base components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,6 +2,9 @@
 import "./styles.css";
 
 // components
-export * from "./components/button/button";
-export * from "./components/input/input";
-export * from "./components/icons/Icons";
+export * from "./components/button";
+export * from "./components/input";
+export * from "./components/Icons";
+export * from "./components/toast";
+export * from "./components/toaster";
+export * from "./components/use-toast";
