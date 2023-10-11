669d71ea787c04423bee5fc0ed343051d417fdee
Krindendo work <lollipopfunkyx9@gmail.com>
2023-10-11 21:19:38 +0200

    update

--- tooling/tsup/index.js
+++ tooling/tsup/index.js
@@ -5,10 +5,10 @@ const config = {
   sourcemap: true,
   dts: true,
   format: ["esm"],
-  async onSuccess() {
-    // emit dts and sourcemaps to enable jump to definition
-    execSync("pnpm tsc --project tsconfig.sourcemap.json");
-  },
+  // async onSuccess() {
+  //   // emit dts and sourcemaps to enable jump to definition
+  //   execSync("pnpm tsc --project tsconfig.sourcemap.json");
+  // },
 };
 
 module.exports = { config };
