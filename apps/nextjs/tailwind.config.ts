52fb21e613a34725f12f8280f941a17de4711311
Krindendo work <lollipopfunkyx9@gmail.com>
2023-10-19 00:18:16 +0200

    fix tailwind config

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@hermes/tailwind-config";
 
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./node_modules/@hermes/ui/components/*.{ts,tsx}",
+  ],
   presets: [baseConfig],
 } satisfies Config;
