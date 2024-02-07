eaff90ceadc3b2921d6677a0f893f85ad7e7e4dd
Krindendo work <lollipopfunkyx9@gmail.com>
2024-02-07 23:58:50 +0100

    feat(auth-proxy): add registration logic

--- apps/auth-proxy/routes/api/auth/register.post.ts
+++ apps/auth-proxy/routes/api/auth/register.post.ts
@@ -1,1 +1,43 @@
-export default eventHandler(async (event) => {});
+import Database from "better-sqlite3";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { eventHandler } from "h3";
+import { z } from "zod";
+
+import { user } from "~/db/schema/user";
+
+const registerSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type RegisterDTO = z.infer<typeof registerSchema>;
+
+const sqlite = new Database("sqlite.db");
+const db = drizzle(sqlite);
+
+export default eventHandler(async (event) => {
+  const body = await readBody<RegisterDTO>(event);
+  const validatedBody = registerSchema.safeParse(body);
+
+  if (!validatedBody.success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Validation of body is failed",
+    });
+  }
+
+  const currentUser = await db
+    .select()
+    .from(user)
+    .where(eq(user.email, body.email));
+
+  if (currentUser[0]) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "user exists",
+    });
+  }
+
+  return "success";
+});
